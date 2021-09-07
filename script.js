1f297e7d38fe8f8fd68dfe8d543213860f12e5db
Lovely Joy Banaybanay <lovelybanaybanay00@gmail.com>
2021-09-07 22:39:00 +0800

    updated design

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		$(this).siblings('span').removeClass();
 		$('.test > div').removeClass();
 		$(".test > div").addClass('kawaii-window kawaii-a');
-		$("#window-name").html("Simple");
+		$("#window-name").html("Soft");
 	});
 	$( "#kawaii-b" ).click(function() {
 		$(this).addClass('active');
@@ -18,6 +18,26 @@ $(document).ready(function() {
 		$(this).siblings('span').removeClass();
 		$('.test > div').removeClass();
 		$(".test > div").addClass('kawaii-window kawaii-c');
-		$("#window-name").html("Retro");
+		$("#window-name").html("Cyberpunk");
+	});
+	$( "#kawaii-d" ).click(function() {
+		$(this).addClass('active');
+		$(this).siblings('span').removeClass();
+		$('.test > div').removeClass();
+		$(".test > div").addClass('kawaii-window kawaii-d');
+		$("#window-name").html("Pink Grunge");
+	});
+	$( "#kawaii-e" ).click(function() {
+		$(this).addClass('active');
+		$(this).siblings('span').removeClass();
+		$('.test > div').removeClass();
+		$(".test > div").addClass('kawaii-window kawaii-e');
+		$("#window-name").html("VIP");
+	});
+
+	$('#lights').click(function () {
+		
+		$('.test').toggleClass("lights-on lights-off"); //you can list several class names 
+      	e.preventDefault();
 	});
 });
