476b89c2ef80b1deb3a2c3d21b4212bce655f23f
Lovely Joy Banaybanay <lovelybanaybanay00@gmail.com>
2021-09-07 21:43:38 +0800

    updated design

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// script
+$(document).ready(function() {
+	$( "#kawaii-a" ).click(function() {
+		$(this).addClass('active');
+		$(this).siblings('span').removeClass();
+		$('.test > div').removeClass();
+		$(".test > div").addClass('kawaii-window kawaii-a');
+		$("#window-name").html("Simple");
+	});
+	$( "#kawaii-b" ).click(function() {
+		$(this).addClass('active');
+		$(this).siblings('span').removeClass();
+		$('.test > div').removeClass();
+		$(".test > div").addClass('kawaii-window kawaii-b');
+		$("#window-name").html("Cloudy");
+	});
+	$( "#kawaii-c" ).click(function() {
+		$(this).addClass('active');
+		$(this).siblings('span').removeClass();
+		$('.test > div').removeClass();
+		$(".test > div").addClass('kawaii-window kawaii-c');
+		$("#window-name").html("Retro");
+	});
+});
