7ffa7fe05751b8acf4805e3899d6216cf2741e33
Lovely Joy Banaybanay <lovelybanaybanay00@gmail.com>
2021-09-08 19:16:50 +0800

    updated design

--- script.js
+++ script.js
@@ -36,8 +36,47 @@ $(document).ready(function() {
 	});
 
 	$('#lights').click(function () {
-		
 		$('.test').toggleClass("lights-on lights-off"); //you can list several class names 
+		$('#lights').toggleClass("active a");
       	e.preventDefault();
 	});
+
+
+
+	$("#addNewButton").click(function(){
+	    var elem = $("<input/>",{
+	        type: "text",
+	        name: "teamName[]",
+	        value: "Ok!"
+	    });
+	    
+	    var removeLink = $("<span/>", {class: "closeinput"}).html("X").click(function(){
+	        $(elem).remove();
+	        $(this).remove();
+	        var index = $( ".closeinput" ).length - 1;
+	        $('.test .kawaii-button').eq(index).remove();
+	    });
+	    $('.test .kawaii-button-box').append('<button class="kawaii-button"> Ok! </button>');
+	    $("#inputs").append(elem).append(removeLink);
+
+
+		});
+		$("#inputs").on("keyup", "input", function(){
+			var btnVal = $(this).val();
+			var index = $(this).index('input');
+			// alert(index);
+			$('.test .kawaii-button').eq(index).html(btnVal);
+		});
+		$("#inputs").on("keyup", "input", function(){
+			var btnVal = $(this).val();
+			var index = $(this).index('#inputs input');
+			// alert(index);
+			$('.test .kawaii-button').eq(index).html(btnVal);
+		});
+		$("#window-input").on("keyup", function(){ $('#window-title').html($(this).val()); });
+		$("#heading-input").on("keyup", function(){ $('#heading').html($(this).val()); });
+		$("#msg-input").on("keyup", function(){ $('.message-content').html($(this).val()); });
 });
+
+
+
